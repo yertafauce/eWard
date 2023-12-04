3b0997327ab05f2636b55856200c592ae8c92fba
chernandez <chernandez@chernandez.grupocestel.local>
2023-12-04 16:18:51 +0100

    ProfMag

---
+++ src/main/java/eWard/repository/ProfesorRepository.java
@@ -0,0 +1,18 @@
+package eWard.repository;
+
+import java.util.List;
+import java.util.Optional;
+
+import eWard.entity.Profesor;
+
+public interface ProfesorRepository<T> {
+
+	public List<Profesor> findAll();
+	
+	public Profesor findByDni(Long id);
+	
+	public Optional<Profesor> findByNombreAndApellidos(String nombre, String apellidos);
+	
+	
+	
+}
