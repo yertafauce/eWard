c1a172a79d3ba5a57ab163d1ec7eb48bfd7be8b7
chernandez <chernandez@chernandez.grupocestel.local>
2023-12-04 14:56:09 +0100

    Inicio Repositories

---
+++ src/main/java/eWard/repository/AlumnoRepository.java
@@ -0,0 +1,11 @@
+package eWard.repository;
+
+
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+@Transactional
+@Component("alumnoMag")
+public class AlumnoRepository {
+
+}
