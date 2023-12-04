a78a28fac5f4cf9fbd57bf2b0f2f3cce5ba9bdcd
Carlos Hernández <chernandez@grupocestel.local>
2023-12-04 12:59:21 +0100

    initial commit

---
+++ src/test/java/eWard/App/EWardApplicationTests.java
@@ -0,0 +1,13 @@
+package eWard.App;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EWardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
