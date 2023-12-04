6e30b79f354fc0c72ffcf99bb3db1afd7bd00714
chernandez <chernandez@chernandez.grupocestel.local>
2023-12-04 13:20:11 +0100

    second push

--- src/main/java/eWard/App/EWardApplication.java
+++ src/main/java/eWard/App/EWardApplication.java
@@ -4,11 +4,15 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
 public class EWardApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EWardApplication.class, args);
+		log.info("hola");
 	}
 
 }
